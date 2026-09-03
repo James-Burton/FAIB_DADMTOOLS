d946220a03572a7ac556fc0ba35b900c95474647
James "Rex" Burton <53842343+James-Burton@users.noreply.github.com>
2026-09-03 14:23:56 -0700

    Update df-to-pg.R
    simplified first function, formatting/style changes

--- R/df-to-pg.R
+++ R/df-to-pg.R
@@ -12,28 +12,40 @@
 #'
 #' @examples df_to_pg(pg_tbl = 'pgTableName', in_df = in_df, pg_conn_params = get_pg_conn_list(), overwrite = 'TRUE', append = 'FALSE')
 
+df_to_pg <- function(
+    pg_tbl,
+    in_df,
+    pg_conn_param,
+    overwrite = TRUE,
+    append = FALSE,
+    output_schema = NULL
+) {
+  in_df <- lowercase_df_colnames(in_df)
 
-df_to_pg <- function(pg_tbl,
-                    in_df,
-                    pg_conn_param,
-                    overwrite = TRUE,
-                    append = FALSE,
-                    output_schema = NULL
-                )
-{
+  if (!is.null(output_schema)) {
+    pg_tbl <- DBI::Id(
+      schema = output_schema,
+      table = pg_tbl
+    )
+  }
 
-  if (is.null(output_schema)){
-    in_df <- lowercase_df_colnames(in_df)}else{
+  conn <- dbConnect(
+    pg_conn_param["driver"][[1]],
+    host = pg_conn_param["host"][[1]],
+    user = pg_conn_param["user"][[1]],
+    dbname = pg_conn_param["dbname"][[1]],
+    password = pg_conn_param["password"][[1]],
+    port = pg_conn_param["port"][[1]]
+  )
 
-      in_df <- lowercase_df_colnames(in_df)
-      pg_tbl <- DBI::Id(schema = output_schema, table =pg_tbl )
-       }
-
-  conn <- dbConnect(pg_conn_param["driver"][[1]],
-                  host = pg_conn_param["host"][[1]],
-                  user = pg_conn_param["user"][[1]],
-                  dbname = pg_conn_param["dbname"][[1]],
-                  password = pg_conn_param["password"][[1]],
-                  port = pg_conn_param["port"][[1]])
   on.exit(RPostgres::dbDisconnect(conn))
-  RPostgres::dbWriteTable(conn, pg_tbl, value = in_df, overwrite = overwrite, append = append, row.names = FALSE)}
+
+  RPostgres::dbWriteTable(
+    conn,
+    pg_tbl,
+    value = in_df,
+    overwrite = overwrite,
+    append = append,
+    row.names = FALSE
+  )
+}
